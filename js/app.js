ae53d9cd73cd71bed7cde87dbaab634757ca2d46
Kevin henry <kevinhenry@mac.com>
2021-01-13 19:19:52 -0800

    Updating JS after lecture

--- js/app.js
+++ js/app.js
@@ -1,20 +1,31 @@
 'use strict';
 
 // linter test - if not used this should have red marks underneath it
+var banana = 42;
 
 // get user name and offer greeting
 var userName = prompt('Hello, what\'s your name?');
+
 alert('Hello ' + userName + ', nice to meet you!');
 
 // write 5 questions. They must accept yes or no or y or n in any case
 // examples: YES, yes, YEs, yeS, yEs, Y, y
+// is this an appropriate name?
 
+// question one
 var questionOne = prompt('Do I live in Seattle?').toLowerCase();
 
-if(questionOne === 'yes' || questionOne === 'y') {
+if (questionOne === 'yes' || questionOne === 'y') {
   // console.log('you are correct');
   alert('you are correct');
 }
-else if(questionOne === 'no' || questionOne === 'n' ){
-  alert('you are WRONG');
-}
+
+// this is not required, but have fun!
+// if(questionOne === 'yes'  || questionOne === 'y') {
+//  // console.log('you are correct');
+// alert('you are correct');
+// } else if ( questionOne === 'no' || questionOne === 'n' ) {
+// alert('you are WRONG, why would you think that');
+// } else {
+// alert('you are WRONG!');
+// }
